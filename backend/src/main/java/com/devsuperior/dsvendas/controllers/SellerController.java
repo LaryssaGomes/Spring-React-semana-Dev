fb7a23d6bb13a9e98fc69ff35a000547210919a4
LaryssaGomes <laryssag533@gmail.com>
2021-05-15 09:23:58 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.service.SellerService;
+
+@RestController // Endica que e um rest
+@RequestMapping(value = "/sellers") // Endicar qual e o caminho do recurso web
+public class SellerController {
+
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll(){
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);// oke responta 400
+		
+	} 
+}
