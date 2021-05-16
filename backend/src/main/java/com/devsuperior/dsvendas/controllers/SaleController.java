215cdf16e0ea7f53ebf3a60df5eb5c2eb565580c
LaryssaGomes <laryssag533@gmail.com>
2021-05-16 11:43:49 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -26,21 +26,21 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
 		Page<SaleDTO> list = service.findAll(pageable);
-		return ResponseEntity.ok(list);// oke responta 400
+		return ResponseEntity.ok(list);// ok e responta 400
 		
 	} 
 	
 	@GetMapping(value = "/amount-by-seller")
 	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
 		List<SaleSumDTO> list = service.amountGroupedBySeller();
-		return ResponseEntity.ok(list);// oke responta 400
+		return ResponseEntity.ok(list);// ok e responta 400
 		
 	} 
 	
 	@GetMapping(value = "/success-by-seller")
 	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
 		List<SaleSuccessDTO> list = service.successGroupedBySeller();
-		return ResponseEntity.ok(list);// oke responta 400
+		return ResponseEntity.ok(list);// ok e responta 400
 		
 	} 
 }
